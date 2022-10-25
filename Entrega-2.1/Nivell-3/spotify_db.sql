3ea04ad0675711d3ba3d192b172a52137c6df87f
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-10-25 09:40:57 +0200

    Coreccions Nivell3

--- Entrega-2.1/Nivell-3/spotify_db.sql
+++ Entrega-2.1/Nivell-3/spotify_db.sql
@@ -5,6 +5,28 @@ CREATE DATABASE spotify_db;
 USE spotify_db;
 
 CREATE TABLE
+    Canço (
+        id INT NOT NULL AUTO_INCREMENT,
+        titol VARCHAR(20) NOT NULL,
+        duracio FLOAT NOT NULL,
+        reproduccions BIGINT NOT NULL,
+        id_playlist INT NOT NULL,
+        id_album INT NOT NULL,
+        id_artista INT NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE Artista (
+    id INT NOT NULL AUTO_INCREMENT,
+    nom VARCHAR(20) NOT NULL,
+    imatge VARCHAR(40) NOT NULL,
+    genere_musical VARCHAR(40) NOT NULL,
+    id_canço INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_artista_canço FOREIGN KEY (id_canço) REFERENCES Canço (id)
+);
+
+CREATE TABLE
     Album (
         id INT NOT NULL AUTO_INCREMENT,
         titol VARCHAR(20) NOT NULL,
@@ -17,31 +39,32 @@ CREATE TABLE
         CONSTRAINT fk_album_canço FOREIGN KEY (id_canço) REFERENCES Canço (id)
     );
 
-CREATE TABLE
-    Artista (
-        id INT NOT NULL AUTO_INCREMENT,
-        nom VARCHAR(20) NOT NULL,
-        imatge VARCHAR(40) NOT NULL,
-        genere_musical VARCHAR(40) NOT NULL,
-        id_canço INT NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_artista_canço FOREIGN KEY (id_canço) REFERENCES Canço (id)
-    );
 
-CREATE TABLE
-    Canço (
-        id INT NOT NULL AUTO_INCREMENT,
-        titol VARCHAR(20) NOT NULL,
-        duracio FLOAT NOT NULL,
-        reproduccions BIGINT NOT NULL,
-        id_playlist INT NOT NULL,
-        id_album INT NOT NULL,
-        id_artista INT NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_canço_playlist FOREIGN KEY (id_playlist) REFERENCES Playlist (id),
-        CONSTRAINT fk_canço_album FOREIGN KEY (id_album) REFERENCES Album (id),
-        CONSTRAINT fk_canço_artista FOREIGN KEY (id_artista) REFERENCES Artista (id)
-    );
+CREATE TABLE Usuari (
+    id INT NOT NULL AUTO_INCREMENT,
+    tipus ENUM('free', 'premium') NOT NULL DEFAULT 'free',
+    sexe ENUM('home', 'dona', 'no-binari', 'trans', 'no-contesta') NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    codi_postal VARCHAR(20) NOT NULL,
+    pais VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Playlist (
+    id INT NOT NULL AUTO_INCREMENT,
+    tipus ENUM('actives', 'esborrades') NOT NULL,
+    id_canço INT NOT NULL,
+    id_usuari INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_playlist_canço FOREIGN KEY (id_canço)
+        REFERENCES Canço (id),
+    CONSTRAINT fk_playlist_usuari FOREIGN KEY (id_usuari)
+        REFERENCES Usuari (id)
+);
+
+
 
 CREATE TABLE
     Like_album (
@@ -53,6 +76,8 @@ CREATE TABLE
         CONSTRAINT fk_like_album_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id)
     );
 
+
+
 CREATE TABLE
     Like_canço (
         id INT NOT NULL AUTO_INCREMENT,
@@ -62,6 +87,34 @@ CREATE TABLE
         CONSTRAINT fk_like_canço_canço FOREIGN KEY (id_canço) REFERENCES Canço (id),
         CONSTRAINT fk_like_canço_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id)
     );
+
+
+CREATE TABLE Targeta (
+    id INT NOT NULL AUTO_INCREMENT,
+    numero_targeta VARCHAR(40) NOT NULL,
+    caducitat DATE NOT NULL,
+    codi_seguretat VARCHAR(20) NOT NULL,
+    id_usuari INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_targeta_usuari FOREIGN KEY (id_usuari)
+        REFERENCES Usuari (id)
+);
+   
+   
+CREATE TABLE Subscripcio (
+    id INT NOT NULL AUTO_INCREMENT,
+    renovacio_automatica BOOLEAN NULL DEFAULT TRUE,
+    pagament ENUM('targeta', 'PayPal') NOT NULL,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL,
+    expires_at DATE NOT NULL,
+    id_usuari INT NOT NULL,
+    id_targeta INT NOT NULL,
+    id_paypal INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_subscripcio_usuari FOREIGN KEY (id_usuari)
+        REFERENCES Usuari (id)
+);
 
 CREATE TABLE
     Pagament (
@@ -83,16 +136,6 @@ CREATE TABLE
         CONSTRAINT fk_paypal_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id)
     );
 
-CREATE TABLE
-    Playlist (
-        id INT NOT NULL AUTO_INCREMENT,
-        tipus ENUM('actives', 'esborrades') NOT NULL,
-        id_canço INT NOT NULL,
-        id_usuari INT NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_playlist_canço FOREIGN KEY (id_canço) REFERENCES Canço (id),
-        CONSTRAINT fk_playlist_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id)
-    );
 
 CREATE TABLE
     Seguir_artista (
@@ -104,130 +147,25 @@ CREATE TABLE
         CONSTRAINT fk_seguir_artista_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id)
     );
 
-CREATE TABLE
-    Subscripcio (
-        id INT NOT NULL AUTO_INCREMENT,
-        renovacio_automatica BOOLEAN NULL DEFAULT True,
-        pagament ENUM('targeta', 'PayPal') NOT NULL,
-        created_at timestamp NULL,
-        updated_at timestamp NULL,
-        expires_at DATE NOT NULL,
-        id_usuari INT NOT NULL,
-        id_targeta INT NOT NULL,
-        id_paypal INT NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_subscripcio_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id),
-        CONSTRAINT fk_subscripcio_targeta FOREIGN KEY (id_targeta) REFERENCES Targeta (id),
-        CONSTRAINT fk_subscripcio_paypal FOREIGN KEY (id_paypal) REFERENCES Paypal (id)
-    );
 
-CREATE TABLE
-    Targeta (
-        id INT NOT NULL AUTO_INCREMENT,
-        numero_targeta VARCHAR(40) NOT NULL,
-        caducitat DATE NOT NULL,
-        codi_seguretat VARCHAR(20) NOT NULL,
-        id_usuari INT NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_targeta_usuari FOREIGN KEY (id_usuari) REFERENCES Usuari (id)
-    );
 
-CREATE TABLE
-    Usuari (
-        id INT NOT NULL AUTO_INCREMENT,
-        tipus ENUM('free', 'premium') NOT NULL DEFAULT 'free',
-        sexe ENUM(
-            'home',
-            'dona',
-            'no-binari',
-            'trans',
-            'no-contesta'
-        ) NOT NULL,
-        username VARCHAR(20) NOT NULL,
-        codi_postal VARCHAR(20) NOT NULL,
-        pais VARCHAR(20) NOT NULL,
-        email VARCHAR(20) NOT NULL,
-        id_playlist INT NOT NULL,
-        PRIMARY KEY (id),
-        CONSTRAINT fk_usuari_playlist FOREIGN KEY (id_playlist) REFERENCES Playlist (id)
-    );
 
-ALTER TABLE Subscripcio
-ADD
-    CONSTRAINT FK_Usuari_TO_Subscripcio FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
-
-ALTER TABLE Targeta
-ADD
-    CONSTRAINT FK_Usuari_TO_Targeta FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
-
-ALTER TABLE Paypal
-ADD
-    CONSTRAINT FK_Usuari_TO_Paypal FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
-
-ALTER TABLE Subscripcio
-ADD
-    CONSTRAINT FK_Targeta_TO_Subscripcio FOREIGN KEY (id_targeta) REFERENCES Targeta (id);
-
-ALTER TABLE Subscripcio
-ADD
-    CONSTRAINT FK_Paypal_TO_Subscripcio FOREIGN KEY (id_paypal) REFERENCES Paypal (id);
-
-ALTER TABLE Seguir_artista
-ADD
-    CONSTRAINT FK_Artista_TO_Seguir_artista FOREIGN KEY (id_artista) REFERENCES Artista (id);
-
-ALTER TABLE Seguir_artista
-ADD
-    CONSTRAINT FK_Usuari_TO_Seguir_artista FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
-
-ALTER TABLE Playlist
-ADD
-    CONSTRAINT FK_Canço_TO_Playlist FOREIGN KEY (id_canço) REFERENCES Canço (id);
-
+    
 ALTER TABLE Canço
 ADD
-    CONSTRAINT FK_Playlist_TO_Canço FOREIGN KEY (id_playlist) REFERENCES Playlist (id);
-
+    CONSTRAINT FK_Playlist_TO_Canço FOREIGN KEY (id_playlist) REFERENCES Playlist (id),
+    ADD
+    CONSTRAINT FK_Album_TO_Canço FOREIGN KEY (id_album) REFERENCES Album (id),
+    ADD
+    CONSTRAINT FK_Artista_TO_Canço FOREIGN KEY (id_artista) REFERENCES Artista (id);
+    
+    
 ALTER TABLE Playlist
 ADD
     CONSTRAINT FK_Usuari_TO_Playlist FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
 
-ALTER TABLE Usuari
+ALTER TABLE Subscripcio
 ADD
-    CONSTRAINT FK_Playlist_TO_Usuari FOREIGN KEY (id_playlist) REFERENCES Playlist (id);
-
-ALTER TABLE Pagament
+    CONSTRAINT FK_Targeta_TO_Subscripcio FOREIGN KEY (id_targeta) REFERENCES Targeta (id),
 ADD
-    CONSTRAINT FK_Subscripcio_TO_Pagament FOREIGN KEY (id_subscripcio) REFERENCES Subscripcio (id);
-
-ALTER TABLE Album
-ADD
-    CONSTRAINT FK_Artista_TO_Album FOREIGN KEY (id_artista) REFERENCES Artista (id);
-
-ALTER TABLE Album
-ADD
-    CONSTRAINT FK_Canço_TO_Album FOREIGN KEY (id_canço) REFERENCES Canço (id);
-
-ALTER TABLE Canço
-ADD
-    CONSTRAINT FK_Album_TO_Canço FOREIGN KEY (id_album) REFERENCES Album (id);
-
-ALTER TABLE Like_canço
-ADD
-    CONSTRAINT FK_Canço_TO_Like_canço FOREIGN KEY (id_canço) REFERENCES Canço (id);
-
-ALTER TABLE Like_canço
-ADD
-    CONSTRAINT FK_Usuari_TO_Like_canço FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
-
-ALTER TABLE Like_album
-ADD
-    CONSTRAINT FK_Album_TO_Like_album FOREIGN KEY (id_album) REFERENCES Album (id);
-
-ALTER TABLE Like_album
-ADD
-    CONSTRAINT FK_Usuari_TO_Like_album FOREIGN KEY (id_usuari) REFERENCES Usuari (id);
-
-ALTER TABLE Canço
-ADD
-    CONSTRAINT FK_Artista_TO_Canço FOREIGN KEY (id_artista) REFERENCES Artista (id);
+    CONSTRAINT FK_Paypal_TO_Subscripcio FOREIGN KEY (id_paypal) REFERENCES Paypal (id);
