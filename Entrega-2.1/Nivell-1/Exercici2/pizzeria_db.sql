9d7288abb3f23572dc6edbfe35818b76f984b5d6
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2022-10-25 15:46:39 +0200

    Insert data Pizzeria

--- Entrega-2.1/Nivell-1/Exercici2/pizzeria_db.sql
+++ Entrega-2.1/Nivell-1/Exercici2/pizzeria_db.sql
@@ -107,8 +107,186 @@ CREATE TABLE
         CONSTRAINT fk_ticket_comanda FOREIGN KEY (id_comanda) REFERENCES Comanda (id)
     );
 
-
-
 /*  Pizzeria:
  Llista quants productes de categoria 'Begudes' s'han venut en una determinada localitat.
  Llista quantes comandes ha efectuat un determinat empleat/da. */
+
+INSERT INTO
+    Botiga (
+        Nom,
+        Adreça,
+        Localitat,
+        Provincia,
+        Codi_postal,
+        Telefon,
+        id_comanda,
+        id_empleat
+    )
+VALUES (
+        'Pizzeria Sabrosona',
+        'Carrer de la Pau',
+        'Barcelona',
+        'Barcelona',
+        '08001',
+        '934567890',
+        1,
+        1
+    ), (
+        'Pizzeria Sabrosilla',
+        'Carrer de la Guerra',
+        'Barcelona',
+        'Barcelona',
+        '08002',
+        '934567890',
+        2,
+        2
+    )
+INSERT INTO
+    Client (
+        Nom,
+        Cognom,
+        Adreça,
+        Codi_postal,
+        Localitat,
+        Provincia,
+        Telefon,
+        id_comanda
+    )
+VALUES (
+        'Pep',
+        'Pepet',
+        'Carrer de la Pau',
+        '08001',
+        'Barcelona',
+        'Barcelona',
+        '934567890',
+        1
+    ), (
+        'Pepa',
+        'Pepota',
+        'Carrer de la Guerra',
+        '08002',
+        'Barcelona',
+        'Barcelona',
+        '934567890',
+        2
+    )
+INSERT INTO
+    Comanda (
+        tipus,
+        Quantitat,
+        created_at,
+        updated_at,
+        id_client,
+        id_botiga
+    )
+VALUES (
+        'botiga',
+        2,
+        '2020-01-01 00:00:00',
+        '2020-01-01 00:00:00',
+        1,
+        1
+    ), (
+        'domicili',
+        1,
+        '2020-01-01 00:00:00',
+        '2020-01-01 00:00:00',
+        2,
+        2
+    )
+INSERT INTO
+    Empleat (
+        Nom,
+        Cognom,
+        NIF,
+        Telefon,
+        tipus,
+        id_botiga,
+        id_ticket
+    )
+VALUES (
+        'Miquel',
+        'Herández',
+        '12345678A',
+        '934567890',
+        'cuiner',
+        1,
+        1
+    ), (
+        'Antonia',
+        'Machada',
+        '12345678B',
+        '934567890',
+        'repartidor',
+        2,
+        2
+    )
+INSERT INTO
+    Producte (
+        Nom,
+        Descripcio,
+        Imatge,
+        Preu,
+        Tipus,
+        id_categoria,
+        id_comanda
+    )
+VALUES (
+        'Pizza Margarita',
+        'Pizza amb tomàquet i mozzarella',
+        'https://www.pizzahut.es/images/pizzas/margarita.png',
+        8.5,
+        'pizza',
+        1,
+        1
+    ), (
+        'Pizza Carbonara',
+        'Pizza amb tomàquet, mozzarella, bacon i ou',
+        'https://www.pizzahut.es/images/pizzas/carbonara.png',
+        9.5,
+        'pizza',
+        1,
+        1
+    ), (
+        'Coca-Cola',
+        'Refresc amb gas',
+        'https://www.coca-cola.es/content/dam/journey/es/es/private/brands/coca-cola/coca-cola.png',
+        1.5,
+        'beguda',
+        2,
+        2
+    )
+INSERT INTO
+    Categoria (Nom, id_producte)
+VALUES ('Pizza', 1), ('Pizza', 2), ('Begudes', 3)
+INSERT INTO
+    Ticket (
+        created_at,
+        updated_at,
+        tipus_pagament,
+        Adreça,
+        Total,
+        canvi_diners,
+        id_empleat,
+        id_comanda
+    )
+VALUES (
+        '2020-01-01 00:00:00',
+        '2020-01-01 00:00:00',
+        'credit',
+        'Carrer de la Pau',
+        18,
+        0,
+        1,
+        1
+    ), (
+        '2020-01-01 00:00:00',
+        '2020-01-01 00:00:00',
+        'app',
+        'Carrer de la Guerra',
+        1.5,
+        0,
+        2,
+        2
+    )
