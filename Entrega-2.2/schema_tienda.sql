46b549b26ec989f1eea3037c5850e0655022873d
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-10-26 09:43:16 +0200

    Més querys

--- Entrega-2.2/schema_tienda.sql
+++ Entrega-2.2/schema_tienda.sql
@@ -59,3 +59,31 @@ SELECT nombre, precio, precio * 1.12 FROM producto;
 -- Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (USD). Utilitza els següents àlies per a les columnes: nom de producto, euros, dòlars.
 
 SELECT nombre AS 'nom de producto', precio AS euros, precio * 1.12 AS dòlars FROM producto;
+
+-- Llista els noms i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
+
+SELECT UPPER(nombre), precio FROM producto;
+
+-- Llista els noms i els preus de tots els productes de la taula producto, convertint els noms a minúscula.
+
+SELECT LOWER(nombre), precio FROM producto;
+
+-- Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant.
+
+SELECT nombre, UPPER(SUBSTRING(nombre, 1, 2)) FROM fabricante;
+
+-- Llista els noms i els preus de tots els productes de la taula producto, arrodonint el valor del preu.
+
+SELECT nombre, ROUND(precio) FROM producto;
+
+-- Llista els noms i els preus de tots els productes de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
+
+SELECT nombre, TRUNCATE(precio, 0) FROM producto;
+
+-- Llista el codi dels fabricants que tenen productes en la taula producto.
+
+SELECT codigo_fabricante FROM producto;
+
+-- Llista el codi dels fabricants que tenen productes en la taula producto, eliminant els codis que apareixen repetits.
+
+SELECT DISTINCT codigo_fabricante FROM producto;
