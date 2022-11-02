e19967bd8dd3413d3f3cb0519a5dc63a3586087e
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-11-02 10:47:26 +0100

    json restaurants fixed

---
+++ Entrega-2.4/script.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require( 'mongodb' );
+
+const url = 'mongodb://localhost:27017';
+
+const client = new MongoClient( url, { useUnifiedTopology: true } );
+
+async function run () {
+  try {
+    await client.connect();
+    const database = client.db( 'restaurants' );
+    const restaurants = database.collection( 'restaurants' );
+
+    // Insertar many restaurants
+
+    const datosRestaurants = restaurants.insertMany([
+
+    ]);
+
+    console.log( 'Datos insertados correctamente' );
+  }
+  catch ( error ) {
+    console.log( error );
+  }
+  finally {
+    await client.close();
+  }
+}
+
+run().catch( console.dir );
