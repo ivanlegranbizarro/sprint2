1ff228aa26d09bb6d5ffcf8a4294cbfac57a0b99
Iván Legrán <ivanlegranbizarro@gmail.com>
2022-11-03 10:21:44 +0100

    més querys a restaurants

--- Entrega-2.4/querys.js
+++ Entrega-2.4/querys.js
@@ -21,15 +21,25 @@ async function run () {
     // Escriu una consulta per mostrar tots els restaurants que estan en el Bronx.
     collection.find( { borough: 'Bronx' } );
     // Escriu una consulta per mostrar els primers 5 restaurants que estan en el Bronx.
+    collection.find( { borough: 'Bronx' } ).limit( 5 );
     // Escriu una consulta per mostrar els 5 restaurants després de saltar els primers 5 que siguin del Bronx.
+    collection.find( { borough: 'Bronx' } ).skip( 5 ).limit( 5 );
     // Escriu una consulta per trobar els restaurants que tenen algun score més gran de 90.
+    collection.find( { grades: { $elemMatch: { score: { $gt: 90 } } } } );
     // Escriu una consulta per trobar els restaurants que tenen un score més gran que 80 però menys que 100.
+    collection.find( { grades: { $elemMatch: { score: { $gt: 80, $lt: 100 } } } } );
     // Escriu una consulta per trobar els restaurants que estan situats en una longitud inferior a -95.754168.
+    collection.find( { 'address.coord': { $lt: -95.754168 } } );
     // Escriu una consulta de MongoDB per a trobar els restaurants que no cuinen menjar 'American ' i tenen algun score superior a 70 i latitud inferior a -65.754168.
+    collection.find( { cuisine: { $ne: 'American ' }, grades: { $elemMatch: { score: { $gt: 70 } } }, 'address.coord': { $lt: -65.754168 } } );
     // Escriu una consulta per trobar els restaurants que no preparen menjar 'American' i tenen algun score superior a 70 i que, a més, es localitzen en longituds inferiors a -65.754168. Nota: Fes aquesta consulta sense utilitzar operador $and.
+    collection.find( { cuisine: { $ne: 'American ' }, grades: { $elemMatch: { score: { $gt: 70 } } }, 'address.coord': { $lt: -65.754168 } } );
     // Escriu una consulta per trobar els restaurants que no preparen menjar 'American ', tenen alguna nota 'A' i no pertanyen a Brooklyn. S'ha de mostrar el document segons la cuisine en ordre descendent.
+    collection.find( { cuisine: { $ne: 'American ' }, grades: { $elemMatch: { grade: 'A' } }, borough: { $ne: 'Brooklyn' } } ).sort( { cuisine: -1 } );
     // Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'Wil' en les tres primeres lletres en el seu nom.
+    collection.find( { name: { $regex: /^Wil/ } }, { projection: { restaurant_id: 1, name: 1, borough: 1, cuisine: 1 } } );
     // Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'ces' en les últimes tres lletres en el seu nom.
+    collection.find( { name: { $regex: /ces$/ } }, { projection: { restaurant_id: 1, name: 1, borough: 1, cuisine: 1 } } );
     // Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per a aquells restaurants que contenen 'Reg' en qualsevol lloc del seu nom.
     // Escriu una consulta per trobar els restaurants que pertanyen al Bronx i preparen plats Americans o xinesos.
     // Escriu una consulta per trobar el restaurant_id, name, borough i cuisine per aquells restaurants que pertanyen a Staten Island, Queens, Bronx o Brooklyn.
